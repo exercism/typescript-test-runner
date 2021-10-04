e26eecb1e0cb1aa4086c7572f3a6896f5886b741
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2021-10-04 22:53:24 +0200

    Re-add window rewrite

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,3 +1,9 @@
+// Required for jest-util to not break during compilation. Normally this is not
+// necessary when using jest, but jest-util is imported in the test runner code
+// which makes it a requirement.
+declare type Window = unknown
+
+// Extensions for jest, as declared in the jest setup (after env) file.
 declare namespace jest {
   interface It {
     task: (taskId: number, ...args: Parameters<jest.It>) => ReturnType<jest.It>
