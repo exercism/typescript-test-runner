74bf5b9f9ede6008c8fbe51e19ebf8f19ce09275
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2024-07-29 21:21:01 +0200

    Explicitely add core-js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 // This file is only used by jest.runner.config.js, when running the
 // test-runner. The tool itself uses typescript's compilation instead.
 module.exports = {
-  presets: ['@exercism/babel-preset-typescript'],
+  presets: [[require('@exercism/babel-preset-typescript'), { corejs: '3.37' }]],
   plugins: [],
 }
