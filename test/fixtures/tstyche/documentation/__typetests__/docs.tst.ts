10237a85ec1589a5ff3eb16df4e94e3a3ce83a14
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2024-07-31 20:32:10 +0200

    Upgrade runner for type-tests wip

---
+++ test/fixtures/tstyche/documentation/__typetests__/docs.tst.ts
@@ -0,0 +1,20 @@
+import { expect, test } from "tstyche";
+
+function firstItem<T>(target: Array<T>): T | undefined {
+  return target[0];
+}
+
+test("first item requires a parameter", () => {
+  expect(firstItem(["a", "b", "c"])).type.toBe<string | undefined>();
+
+  // @ts-expect-error
+  expect(firstItem()).type.toRaiseError("Expected 1 argument");
+});
+
+function secondItem<T>(target: Array<T>): T | undefined {
+  return target[1];
+}
+
+test("handles numbers", () => {
+  expect(secondItem([1, 2, 3])).type.toBe<number | undefined>();
+});
---
+++ test/fixtures/tstyche/fire/__typetests__/fire.tst.ts
@@ -0,0 +1,14 @@
+import { test, expect, describe } from 'tstyche'
+import type { MethodLikeKeys } from '../fire.js'
+
+interface Sample {
+  description: string
+  getLength: () => number
+  getWidth?: () => number
+}
+
+describe('fire', () => {
+  test('all method keys are found', () => {
+    expect<MethodLikeKeys<Sample>>().type.toBe<'getLength' | 'getWidth'>()
+  })
+})
